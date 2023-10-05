4d975b685cab2e143311224c9b0a2f3e38dc6edc
k1shor <k1shor@hotmail.com>
2023-10-05 14:28:15 +0545

    register function

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
 export const API = process.env.REACT_APP_BACKEND_URL 
+export const API_USER = process.env.REACT_APP_BACKEND_URL + '/user'
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,25 +1,67 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { register } from '../api/userApi'
 
 const Register = () => {
-  return (
-    <div style={{minHeight: '79vh'}}>
-     <div className='w-1/2 p-5 bg-slate-200 m-auto my-3 rounded-lg'>
-        <h1 className='text-3xl text-center font-bold underline mb-3'>Register</h1>
-        <label htmlFor="username" className='text-xl text-left'>Username</label>
-        <input type="text" id="username" className='w-full  outline-slate-500 px-2 py-1 rounded' />
-        <label htmlFor="email" className='text-xl text-left'>E-mail</label>
-        <input type="text" id="email" className='w-full  outline-slate-500 px-2 py-1 rounded' />
-        <label htmlFor="pwd" className='text-xl text-left'>Password</label>
-        <input type="password" id="pwd" className='w-full  outline-slate-500 px-2 py-1 rounded' />
-        <button className='bg-blue-500 w-full my-2 py-1 rounded text-white'>Register</button>
-        <span>Already not have an account? <Link to='/login' className='text-blue-800 hover: text-blue-900 hover:bg-gray-400 active:text-blue-950 px-2 py-1 rounded hover:text-white'>Login</Link></span>
-        
-        
-        
-    </div>   
-    </div>
-  )
+    let [username, setUsername] = useState('')
+    let [email, setEmail] = useState('')
+    let [password, setPassword] = useState('')
+
+    let [error, setError] = useState('')
+    let [success, setSuccess] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        register(username, email, password)
+            .then(data => {
+                if (data.error) {
+                    setError(data.error)
+                    setSuccess(false)
+                }
+                else {
+                    setError('')
+                    setSuccess(true)
+                }
+            })
+    }
+    const showError = () => {
+        if (error) {
+            return <div className='bg-red-200 text-center text-3xl py-2 text-red-600'>{error}</div>
+        }
+    }
+    const showSuccess = () => {
+        if (success) {
+            return <div className='bg-green-200 text-center text-3xl py-2 text-green-600'>
+                User Registered Successfully
+            </div>
+        }
+    }
+
+
+    return (
+        <div style={{ minHeight: '79vh' }}>
+            {showSuccess()}
+            <form className='w-1/2 p-5 bg-slate-200 m-auto my-3 rounded-lg'>
+                <h1 className='text-3xl text-center font-bold underline mb-3'>Register</h1>
+            {showError()}
+                <label htmlFor="username" className='text-xl text-left'>Username</label>
+                <input type="text" id="username" className='w-full  outline-slate-500 px-2 py-1 rounded'
+                    onChange={(event) => {
+                        setUsername(event.target.value)
+                    }} />
+                <label htmlFor="email" className='text-xl text-left'>E-mail</label>
+                <input type="text" id="email" className='w-full  outline-slate-500 px-2 py-1 rounded'
+                    onChange={e => setEmail(e.target.value)} />
+                <label htmlFor="pwd" className='text-xl text-left'>Password</label>
+                <input type="password" id="pwd" className='w-full  outline-slate-500 px-2 py-1 rounded'
+                    onChange={e => setPassword(e.target.value)} />
+                <button className='bg-blue-500 w-full my-2 py-1 rounded text-white'
+                    onClick={handleSubmit}>
+                    Register</button>
+                <span>Already not have an account? <Link to='/login' className='text-blue-800 hover: text-blue-900 hover:bg-gray-400 active:text-blue-950 px-2 py-1 rounded hover:text-white'>Login</Link></span>
+            </form>
+        </div>
+    )
 }
 
 export default Register
